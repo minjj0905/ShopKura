a00a9afa52dd6515246bc8512511774f82cb4b98
minjj0905 <kmj2967@kookmin.ac.kr>
2020-10-23 06:10:10 +0900

    Feat main to basket - 장바구니로 이동할 때 선택한 상품 전달 - 장바구니에서 선택한 상품 개수, 총 금액 표시

--- ShopKura/app/src/main/java/com/example/shopkura/BasketActivity.java
+++ ShopKura/app/src/main/java/com/example/shopkura/BasketActivity.java
@@ -1,8 +1,19 @@
 package com.example.shopkura;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class BasketActivity extends AppCompatActivity {
 
@@ -10,5 +21,73 @@ public class BasketActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_basket);
+
+// 시작될때 ----------
+
+
+//        상단 홈버튼
+        Button home_button = (Button)findViewById(R.id.button_home);
+
+//        FlowerLayout 배열
+        ConstraintLayout layout_one = (ConstraintLayout)findViewById(R.id.layout_hydra);
+        ConstraintLayout layout_two = (ConstraintLayout)findViewById(R.id.layout_fog);
+        ConstraintLayout layout_three = (ConstraintLayout)findViewById(R.id.layout_rose);
+        ConstraintLayout layout_four = (ConstraintLayout)findViewById(R.id.layout_set);
+        ConstraintLayout layout_five = (ConstraintLayout)findViewById(R.id.layout_pink);
+        ConstraintLayout[] flowerLayouts = {layout_one, layout_two, layout_three, layout_four, layout_five};
+
+//        CheckBox 배열
+        CheckBox checkBox_one = (CheckBox)findViewById(R.id.checkBox_hydra);
+        CheckBox checkBox_two = (CheckBox)findViewById(R.id.checkBox_fog);
+        CheckBox checkBox_three = (CheckBox)findViewById(R.id.checkBox_rose);
+        CheckBox checkBox_four = (CheckBox)findViewById(R.id.checkBox_set);
+        CheckBox checkBox_five = (CheckBox)findViewById(R.id.checkBox_pink);
+        CheckBox[] flowerCheckBoxes = {checkBox_one, checkBox_two, checkBox_three, checkBox_four, checkBox_five};
+
+//        Empty TextView
+        TextView emptyText = (TextView)findViewById(R.id.empty_text);
+
+//        선택된 상품 개수
+        TextView select_cnt = (TextView)findViewById(R.id.select_cnt_text);
+//        총가격
+        TextView total_price = (TextView)findViewById(R.id.total_price_text);
+
+//        total layout
+        LinearLayout total_layout = (LinearLayout)findViewById(R.id.total_layout);
+
+//        가격 배열
+        int[] price = {16000, 15000, 21000, 24000, 21000};
+
+        Intent main_intent = getIntent();
+        boolean isSelected = main_intent.getExtras().getBoolean("isSelected");
+
+        if(isSelected) {
+            ArrayList<Integer> selectFlower = main_intent.getExtras().getIntegerArrayList("selectFlowers");
+            emptyText.setVisibility(View.GONE);
+            total_layout.setVisibility(View.VISIBLE);
+            int sum = 0;
+            for(int i=0; i<selectFlower.size(); i++) {
+                flowerLayouts[selectFlower.get(i)].setVisibility(View.VISIBLE);
+                flowerCheckBoxes[selectFlower.get(i)].setChecked(true);
+                sum += price[i];
+            }
+            select_cnt.setText(""+selectFlower.size());
+            total_price.setText(""+sum);
+
+        } else {
+            emptyText.setVisibility(View.VISIBLE);
+            total_layout.setVisibility(View.GONE);
+        }
+
+
+//        상단 홈버튼 클릭시 홈으로 이동
+        home_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+
     }
 }
--- ShopKura/app/src/main/java/com/example/shopkura/MainActivity.java
+++ ShopKura/app/src/main/java/com/example/shopkura/MainActivity.java
@@ -6,6 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,14 +18,82 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//        상단의 장바구니 버튼
         Button basket_button = (Button)findViewById(R.id.go_basket);
 
+//        하단 버튼들
+        Button get_basket_button = (Button)findViewById(R.id.get_basket);
+        Button buy_button = (Button)findViewById(R.id.buy);
+
+//        꽃 checkbox 배열
+        CheckBox checkBox_hydra = (CheckBox)findViewById(R.id.checkBox_hydra);
+        CheckBox checkBox_fog = (CheckBox)findViewById(R.id.checkBox_fog);
+        CheckBox checkBox_rose = (CheckBox)findViewById(R.id.checkBox_rose);
+        CheckBox checkBox_set = (CheckBox)findViewById(R.id.checkBox_set);
+        CheckBox checkBox_pink = (CheckBox)findViewById(R.id.checkBox_pink);
+        CheckBox[] checkBoxes_flower = {checkBox_hydra, checkBox_fog, checkBox_rose,
+                checkBox_set, checkBox_pink};
+
+//        상단 장바구니 버튼 클릭시
         basket_button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void  onClick(View v) {
+            public void onClick(View v) {
                 Intent basket_intent = new Intent(getApplicationContext(), BasketActivity.class);
+                boolean flowerSelectCheck = isFlowerSelected(checkBoxes_flower);
+                basket_intent.putExtra("isSelected", flowerSelectCheck);
+//                선택된 꽃 있는지 확인하고 넘김
+                if(flowerSelectCheck) {
+                    ArrayList<Integer> selectFlower = getSelectFlowerNumber(checkBoxes_flower);
+                    basket_intent.putExtra("selectFlowers", selectFlower);
+                }
+
                 startActivity(basket_intent);
             }
         });
+
+//        하단 장바구니 버튼 클릭시
+        get_basket_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                선택된 체크박스가 있는지 확인
+                if(isFlowerSelected(checkBoxes_flower)) {
+//                    있다면 선택된 것들 확인하고 basket 에 넘김
+                    ArrayList<Integer> selectFlower = getSelectFlowerNumber(checkBoxes_flower);
+                    Intent basket_intent = new Intent(getApplicationContext(), BasketActivity.class);
+                    boolean flowerSelectCheck = isFlowerSelected(checkBoxes_flower);
+                    basket_intent.putExtra("isSelected", flowerSelectCheck);
+                    basket_intent.putExtra("selectFlowers", selectFlower);
+                    startActivity(basket_intent);
+                }
+                else{
+//                    장바구니 버튼을 눌렀을 때 선택된 체크박스가 없다면 Toast 출력
+                    Toast toast = Toast.makeText(getApplicationContext(), "선택된 상품이 없습니다!",
+                                                    Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            }
+        });
     }
+
+    //꽃이 선택되었는지 확인하는 함수
+    public boolean isFlowerSelected(CheckBox[] checkBoxes) {
+        for(int i=0; i<checkBoxes.length; i++) {
+            if(checkBoxes[i].isChecked()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+//    선택된 꽃의 번호를 반환하는 함수
+    public ArrayList<Integer> getSelectFlowerNumber(CheckBox[] checkBoxes) {
+        ArrayList<Integer> selectFlowerNumber = new ArrayList<>();
+        for(int i=0; i<checkBoxes.length; i++) {
+            if(checkBoxes[i].isChecked()) {
+                selectFlowerNumber.add(i);
+            }
+        }
+        return selectFlowerNumber;
+    }
+
 }
